ea692db59167638ea84155d7a5bfcdd150aecc33
Amirhossein Bakhshayeshiyan <bamirhossein65@gmail.com>
2026-06-15 19:23:03 +0330

    fix some problem

--- backend/src/auth/admin_auth.py
+++ backend/src/auth/admin_auth.py
@@ -2,13 +2,14 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
 from sqlalchemy.orm import Session
+
 from src.config.database import get_db
 from src.config.config import settings
 from src.models.admin import Admin
 
-Oauth_acheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login/")
 
-def get_current_admin(token: str = Depends(Oauth_acheme), db: Session = Depends(get_db)):
+def get_current_admin(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="توکن نامعتبر است یا منقضی شده لطفا مجددا وارد شوید",
@@ -17,11 +18,9 @@ def get_current_admin(token: str = Depends(Oauth_acheme), db: Session = Depends(
     
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
-
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
-
     except JWTError:
         raise credentials_exception
     
